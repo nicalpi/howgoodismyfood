1e53f6080a9e360bf90e8b059a6f1bab43506dcd
Theo Cushion <theo@jivatechnology.com>
2009-12-04 22:53:45 +0000

    Feature: Added basic model validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,37 @@
 class Product < ActiveRecord::Base
+  
+  validates_presence_of   :name
+  validates_uniqueness_of :barcode
+  validates_format_of     :barcode, :with => /\A[0-9A-Z]+\Z/
+  
+  validates_inclusion_of :kind, :in => %w( food drink )
+  
+  validates_inclusion_of :energy,  :in => 0..10000
+  validates_inclusion_of :portion, :in => 0..1000
+  
+  validates_inclusion_of :protein,      :in => 0..100
+  validates_inclusion_of :carbohydrate, :in => 0..100
+  validates_inclusion_of :sugar,        :in => 0..100
+  validates_inclusion_of :fat,          :in => 0..100
+  validates_inclusion_of :saturate,     :in => 0..100
+  validates_inclusion_of :fibre,        :in => 0..100
+  validates_inclusion_of :sodium,       :in => 0..100
+  validates_inclusion_of :added_sugar,  :in => 0..100, :if => :added_sugars_needed?
+
+  validate :added_sugar_amount_correct?
+  
+  def barcode=(code)
+    write_attribute(:barcode, code.upcase.gsub(/[ \-]/,"") )
+  end
+
+private
+  
+  def added_sugar_amount_correct?
+    errors.add :added_sugar, 'must be less than total sugars' if added_sugar && (added_sugar > sugar) 
+  end
+  
+  def added_sugars_needed?
+    sugar && sugar > 5
+  end
+  
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,64 @@
-require 'test_helper'
+require File.join( File.dirname(File.dirname(__FILE__)), 'test_helper' )
 
 class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  subject { @product = Factory :product }
+
+  should_allow_values_for       :barcode, "1231 abC 123", "2064-24503-2", "6 410500 046431"
+  should_not_allow_values_for   :barcode, "1231 ABC 123!", "|2064-24503-2"
+  should_validate_uniqueness_of :barcode  
+
+  should_allow_values_for     :kind, "food", "drink"
+  should_not_allow_values_for :kind, "bar"
+  
+  should_ensure_value_in_range :energy,  0..10000
+  should_ensure_value_in_range :portion, 0..1000
+  
+  should_ensure_value_in_range :protein,      0..100
+  should_ensure_value_in_range :carbohydrate, 0..100
+  should_ensure_value_in_range :sugar,        0..100
+  should_ensure_value_in_range :fat,          0..100
+  should_ensure_value_in_range :saturate,     0..100
+  should_ensure_value_in_range :fibre,        0..100
+  should_ensure_value_in_range :sodium,       0..100
+  should_ensure_value_in_range :added_sugar,  0..100
+
+  context "barcode input" do
+    should "upcase" do
+      product = Product.new :barcode => "aBce"    
+      assert_equal "ABCE", product.barcode
+    end
+    
+    should "remove spaces and hyphens" do
+      product = Product.new :barcode => "12 3-4"      
+      assert_equal "1234", product.barcode
+    end
   end
+
+  context "validating added_sugar" do  
+    context "when added_sugar is not present" do
+      should "be invalid if total sugar is greater than 5g" do
+        @product = Factory.build :product, :sugar => 6, :added_sugar => nil
+        assert !@product.valid?
+      end
+    
+      should "be valid if total sugar is =< 5g" do
+        @product = Factory.build :product, :sugar => 5, :added_sugar => nil
+        assert @product.valid?
+      end
+    end
+    
+    context "when added_sugar is present" do
+      should "be invalid if greater than total sugar" do
+        @product = Factory.build :product, :sugar => 6, :added_sugar => 7
+        assert !@product.valid?
+      end
+      
+      should "be valid if =< than total sugar" do
+        @product = Factory.build :product, :sugar => 6, :added_sugar => 6
+        assert @product.valid?
+      end
+    end
+  end
+
 end
