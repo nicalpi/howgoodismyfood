a843d002aba99a8b0791b7f3ba7257d4d7c97034
nicolas <nicolas.alpi@gmail.com>
2009-12-05 03:24:04 +0000

    Fomr product and product display First shot of form
    Form and bg layout
    
    Product display red/amber/green image

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,32 @@
 class ProductsController < ApplicationController
 
   def index
-    
+    @product = Product.find_by_barcode(params[:q])
+    if @product.blank?
+      @product = Product.new
+      render :new
+    else
+      render :show
+    end
   end
 
+  def new
+    @product = Product.new
+  end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    
+    if @product.save
+      flash[:notice] = "Your product is now saved"
+      redirect_to product_path(@product)
+    else
+      flash[:error] = "Seems something went wrong!"
+      render :new
+    end
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+jQuery(document).ready(function(){
+
+  $("input[type='radio'][name='product[kind]']").click(function(){
+    if($(this).val() == "food") {
+      //Type is food so the unit needs to be gr
+      $(".per").each(function(){
+        $(this).html($(this).html().replace(/ml/,"g"));  
+      });
+    }
+    else{
+      //Type is drink so the unit needs to be ml
+      $(".per").each(function(){
+        $(this).html($(this).html().replace(/g/,"ml"));  
+      });
+    }
+    
+    
+  });
+
+});
+
+
