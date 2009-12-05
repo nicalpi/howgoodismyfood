f6a480fdd91741a94031b25b06da96b11ff76c06
Theo Cushion <theo@jivatechnology.com>
2009-12-05 00:55:22 +0000

    Feature: Added FSA traffic light rating for foods

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,8 +23,63 @@ class Product < ActiveRecord::Base
   def barcode=(code)
     write_attribute(:barcode, code.upcase.gsub(/[ \-]/,"") )
   end
+  
+  def fsa
+    {
+      :fat      => fsa_fat,
+      :sugar    => fsa_sugar,
+      :saturate => fsa_saturate,
+      :sodium   => fsa_sodium
+    }
+  end
 
 private
+  
+  def fsa_fat
+    if (portion / 100) * fat > 21
+      :red
+    else
+      case fat
+        when 0..3  : :green
+        when 3..20 : :amber
+        else :red
+      end
+    end
+  end
+  
+  def fsa_saturate
+    if (portion / 100) * saturate > 6
+      :red
+    else
+      case saturate
+        when 0..1.5 : :green
+        when 1.5..5 : :amber
+        else :red
+      end
+    end
+  end
+  
+  def fsa_sugar
+    if sugar <= 5
+      :green
+    elsif  ((portion > 100 && (portion / 100) * added_sugar <= 15) || portion <= 100) && added_sugar <= 12.5
+      :amber
+    else
+      :red
+    end
+  end
+  
+  def fsa_sodium
+    if (portion / 100) * sodium > 2.4
+      :red
+    else
+      case sodium
+        when 0..0.3 : :green
+        when 0.3..1.5 : :amber
+        else :red
+      end
+    end
+  end
   
   def added_sugar_amount_correct?
     errors.add :added_sugar, 'must be less than total sugars' if added_sugar && (added_sugar > sugar) 
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -60,5 +60,93 @@ class ProductTest < ActiveSupport::TestCase
       end
     end
   end
+  
+  context "FSA Traffic light signpost labelling for" do
+    context "drink" do
+    
+    end
+    
+    context "food" do
+      context "testing sugar" do
+        should "be green for < 5 sugar" do
+          assert_equal :green, Factory.build(:product, :sugar => 4).fsa[:sugar]
+        end
+        
+        should "be red for portion > 100g and added_sugar > 15g / portion" do
+          assert_equal :red, Factory.build(:product, :portion => 101, :sugar => 16, :added_sugar => 16).fsa[:sugar]
+        end
+        
+        should "be red for portion > 100g and added_sugar < 15g / portion but added_sugar > 12.5 / 100g" do
+          assert_equal :red, Factory.build(:product, :portion => 101, :sugar => 14, :added_sugar => 14).fsa[:sugar]
+        end
+        
+        should "be amber for portion > 100g and added_sugar < 15g / portion and added_sugar < 12.5 / 100g" do
+          assert_equal :amber, Factory.build(:product, :portion => 101, :sugar => 12, :added_sugar => 12).fsa[:sugar]
+        end
+        
+        should "be red for portion < 100g and added_sugar > 12.5 / 100g" do
+          assert_equal :red, Factory.build(:product, :portion => 99, :sugar => 13, :added_sugar => 13).fsa[:sugar]
+        end
+        
+        should "be amber for portion < 100g and added_sugar < 12.5 / 100g" do
+          assert_equal :amber, Factory.build(:product, :portion => 99, :sugar => 10, :added_sugar => 10).fsa[:sugar]
+        end
+        
+      end
+      
+      context "with examples" do
+        setup do
+          @examples = [
+            # Product 1 - Ready meal
+            { :nutrition => { :fat => 2.2, :saturate => 0.4, :sugar => 1.5, :sodium => 0.35, :portion => 400 }, 
+              :banding   => { :fat => :green, :saturate => :green, :sugar => :green, :sodium => :amber } },
+            # Product 2 - Ready meal
+            { :nutrition => { :fat => 6, :saturate => 0.4, :sugar => 1.5, :sodium => 0.35, :portion => 400 }, 
+              :banding   => { :fat => :red, :saturate => :green, :sugar => :green, :sodium => :amber } },
+            # Product 3 - Sandwich
+            { :nutrition => { :fat => 8.4, :saturate => 1.8, :sugar => 2.9, :sodium => 0.5, :portion => 180 }, 
+              :banding   => { :fat => :amber, :saturate => :amber, :sugar => :green, :sodium => :amber } },
+            # Product 4 - Breakfast cereal (e.g. wheat biscuits)
+            { :nutrition => { :fat => 2.5, :saturate => 0.5, :sugar => 0.9, :sodium => 0.02, :portion => 45 }, 
+              :banding   => { :fat => :green, :saturate => :green, :sugar => :green, :sodium => :green } },
+            # Product 5 - Breakfast cereal (e.g. high fruit muesli)
+            { :nutrition => { :fat => 3.0, :saturate => 0.7, :sugar => 29.4, :added_sugar => 0, :sodium => 0.04, :portion => 50 }, 
+              :banding   => { :fat => :green, :saturate => :green, :sugar => :amber, :sodium => :green } },
+            # Product 6 - Breakfast cereal (e.g. flakes, nuts and fruit)
+            { :nutrition => { :fat => 4.6, :saturate => 0.6, :sugar => 25.9, :added_sugar => 14.5, :sodium => 0.64, :portion => 40 }, 
+              :banding   => { :fat => :amber, :saturate => :green, :sugar => :red, :sodium => :amber } }
+          ]
+        end
+        
+        should "return correct fat traffic light" do
+          @examples.each do |e|
+            p = Product.new(e[:nutrition])
+            assert_equal e[:banding][:fat], p.fsa[:fat]
+          end
+        end
+
+        should "return correct saturates traffic light" do
+          @examples.each do |e|
+            p = Product.new(e[:nutrition])
+            assert_equal e[:banding][:saturate], p.fsa[:saturate]
+          end
+        end
+
+        should "return correct sugars traffic light" do
+          @examples.each do |e|
+            p = Product.new(e[:nutrition])
+            assert_equal e[:banding][:sugar], p.fsa[:sugar]
+          end
+        end
+
+        should "return correct sodium traffic light" do
+          @examples.each do |e|
+            p = Product.new(e[:nutrition])
+            assert_equal e[:banding][:sodium], p.fsa[:sodium]
+          end
+        end
+      end
+    end
+  end
 
 end
